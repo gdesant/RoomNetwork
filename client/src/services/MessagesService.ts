555bbc0ca36a77838917f4fb666ccf2228d6106f
gdesant <Lesaint-7860078>
2022-05-31 16:45:20 +0200

    chatPop Websocket + anim

--- client/src/services/MessagesService.ts
+++ client/src/services/MessagesService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const authUrl =  "http://192.168.1.10:5000/auth"//process.env.API_URL ||'https://hexwar-dev.herokuapp.com/';
-const msgUrl =  "http://192.168.1.10:5000/messages"//process.env.API_URL ||'https://hexwar-dev.herokuapp.com/';
+const authUrl =  "http://192.168.1.15:5000/auth"//process.env.API_URL ||'https://hexwar-dev.herokuapp.com/';
+const msgUrl =  "http://192.168.1.15:5000/messages"//process.env.API_URL ||'https://hexwar-dev.herokuapp.com/';
 
 class MessagesService {
     static async sendRoomMessage(userId: number, roomId: number, content: string) {
@@ -20,7 +20,7 @@ class MessagesService {
     static async sendNewRoomMessage(id1: number, id2: number, content: string) {
         console.log("        SendNewRoomMessage - MessagesService")
         try {
-            const res = await axios.post(authUrl + "/msg/new2" , {
+            const res = await axios.post(authUrl + "/msg/neR" , {
                 ownerId: id1,
                 userId: id2,
                 content: content
--- client/src/views/DashBoardViews/chatsViews/chatPop.vue
+++ client/src/views/DashBoardViews/chatsViews/chatPop.vue
@@ -6,59 +6,81 @@
           <fa  size="xl"  icon="plus"/>
         </div>
       </div>
-      <div v-for="chat in chats.filter(element => element.isMinus == true)" :key="chat.name" class="table chatAddIco chatSettIco b-clr-1">
-        <div class="table-c">
-          <img class="chatImg"  height="50" width="50" :src="chat.url">
+      <transition-group name="listIco">
+        <div v-for="chat in sidedChats" :key="chat.name" class="table chatAddIco chatSideIco b-clr-1">
+          <div class="table-c pe-auto " @click="sideChat(chat)">
+            <img class="chatImg" height="55" width="55" :src="chat.url">
+          </div>
         </div>
-      </div>
+      </transition-group>
+
       <div class="table chatAddIco chatSettIco b-clr-1">
         <div class="table-c">
           <fa  icon="ellipsis"/>
         </div>
       </div>
     </div>
-    <div  class="chatPop"  :class="chat.isMinus == true ? 'downChatPop'  : 'upChatPop' " v-for="chat in chats" :key="chat.name">
-      <div class="fx-r wp100 hp10" style="background-color: var(--main-color);text-align: center">
-        <div class="wp70 hp100 tal">
-          <h3  class="pop00 mop00 mlp25">{{chat.name}}</h3>
-        </div>
-        <div class="wp30 hp100">
-          <div  class="table wp90 hp50 tar pe-n">
-            <div class="table-c pe-auto" @click="minusChat(chat)">
-              <fa icon="minus"/>
+    <div class="fx-rr chatPotDiv">
+      <transition-group name="list">
+        <div  class="chatPop list-item"  :class="[chat.isMinus == true ? 'downChatPop'  : '', chat.isSided == true ? 'sideChat' : '']" v-for="chat in chats" :key="chat.name">
+          <div class="fx-r wp100 hp10" style="background-color: var(--main-color);text-align: center">
+            <div class="wp70 hp100 tal">
+              <h3  class="pop00 mop00 mlp25">{{chat.name}}</h3>
             </div>
-            <div class="table-c pe-auto">
-              <fa icon="sliders"/>
-            </div>
-            <div class="table-c pe-auto" @click="closeChat(chat)">
-              <fa icon="xmark"/>
-            </div>
+            <div class="wp30 hp100">
+              <div  class="table wp90 hp50 pe-n">
+                <div class="table-c pe-auto tar transition01 scaleHov12" @click="minusChat(chat)">
+                  <fa icon="minus"/>
+                </div>
+                <div class="table-c pe-auto tar  transition01 scaleHov12">
+                  <fa icon="sliders"/>
+                </div>
+                <div class="table-c pe-auto tar  transition01 scaleHov12" @click="closeChat(chat)">
+                  <fa icon="xmark"/>
+                </div>
 
-          </div>
-        </div>
-      </div>
-      <div class="fx-c wp100 hp75" style="background-color: var(--fourth-color)">
-        {{chat.data}}
-      </div>
-      <div class="fx-r wp100 hp15" style="background-color: var(--third-color)">
-        <div class="wp90 hp100">
-          <input class="wp100 hp100">
-        </div>
-        <div  class="table wp10 hp100">
-          <div  class="table-c">
-            <fa icon="message" />
-          </div>
-        </div>
+              </div>
+              <div  class="table wp90 hp50  pe-n">
+                <div class="table-c pe-auto tar  transition01 scaleHov12" @click="sideChat(chat)">
+                  <fa icon="arrow-right-to-bracket"/>
+                </div>
 
-      </div>
+              </div>
+            </div>
+          </div>
+          <div class="fx-c wp100 hp90">
+            <div class="fx-c wp100 hinp50 haxp95 messagesContainer" style="background-color: var(--fourth-color); overflow-x: hidden; overflowy: auto">
+              {{chat.data}}
+              {{chat.data}}
+              {{chat.data}}
+            </div>
+            <div class="fx-r wp100 b-clr-1" style="border-top: 3px solid var(--main-color); height: initial">
+              <div class="wp90 mrp10" style="height: initial;">
+                <textarea class="wp100 clr-wht transition02 taMsg" v-model="chat.textInput" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"' onfocusin='this.style.height = "";this.style.height = this.scrollHeight + "px"' type="text" maxlength="250"></textarea>
+              </div>
+              <div  class="table wp10 mlp50 mrp50 hp100">
+                <div  class="table-c  pe-auto transition02 sendEffect clr-3" @click="sendMessage(chat)">
+                  <fa icon="paper-plane" size="xl"/>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </transition-group>
     </div>
+
+
   </div>
 </template>
 
 <script>
 import RoomsService from "@/services/RoomsService";
 import UsersService from "@/services/UsersService";
+import MessagesService from "@/services/MessagesService";
 
+//const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default {
   name: "ChatPop",
@@ -69,37 +91,51 @@ export default {
   data(){
     return{
       chats: new Array(),
+      sidedChats: new Array(),
     }
   },
   created(){
     this.emitter.on("openChatWith", this.openChat);
   },
   methods:{
+    initChat(data, type){
+      let Room = {};
+
+      if (type != false)
+      {
+        Room.name = data.Members.find(element => element.id !== this.$props.userid).username;
+        Room.url = data.Members.find(element => element.id !== this.$props.userid).pp_url;
+
+      } else {
+        Room.name = data.username;
+        Room.url = data.pp_url;
+      }
+
+      Room.isMinus = false;
+      Room.isSided = false;
+      Room.textInput = "";
+      Room.ptype = type;
+      Room.data = data;
+
+      return Room
+    },
     async openChat(data){
       const Rdata = await RoomsService.getPrivateRoom(this.$props.userid, data.id)
       let Room = {};
 
-      if (Rdata != false)
-      {
-        Room.name = Rdata.Members.find(element => element.id !== this.$props.userid).username;
-        Room.url = Rdata.Members.find(element => element.id !== this.$props.userid).pp_url;
-        Room.ptype = 0;
-        Room.data = Rdata;
+      if (Rdata != false){
+        Room = this.initChat(Rdata,true)
+        this.$props.socket.emit('join_room', {id: Room.data.id})
       }
       else
       {
-        const User = await  UsersService.getUserById(data.id)
-        Room.name = User.username;
-        Room.url = User.pp_url;
-        Room.ptype = 1;
-        Room.data = User;
+        const User = await UsersService.getUserById(data.id)
+        Room = this.initChat(User, false)
       }
 
       if (Room  != {})
       {
-        Room.isMinus = false;
-
-        if(this.$data.chats.find(element => element.name == Room.name) === undefined)
+        if(this.$data.chats.find(element => element.name == Room.name) === undefined && this.$data.sidedChats.find(element => element.name == Room.name) === undefined)
         {
           console.log(Room)
           this.$data.chats.push(Room)
@@ -110,12 +146,61 @@ export default {
       }
       console.log("Can't  find  chat !")
     },
+    async sendMessage(data){
+      let msg = null;
+
+      if (data.ptype == true)
+        msg = await MessagesService.sendRoomMessage(this.$props.userid, data.data.id, data.textInput)
+      else
+        msg = await MessagesService.sendNewRoomMessage(this.$props.userid, data.data.id, data.textInput)
+
+      console.log("Envoie de nouveau message :")
+      console.log(msg)
+
+    },
+    sideChat(data){
+
+      let sendchat = null;
+      let receivechat = null;
+
+
+      if (data.isSided == true) //Unside
+      {
+        sendchat = this.$data.sidedChats;
+        receivechat = this.$data.chats;
+      }
+      else //Side
+      {
+        sendchat = this.$data.chats;
+        receivechat = this.$data.sidedChats;
+      }
+
+      let index = sendchat.indexOf(data)
+      sendchat[index].isSided = !sendchat[index].isSided;
+
+      receivechat.splice(receivechat.length, 0, sendchat[index]);
+      sendchat.splice(index,1);
+
+      if (data.isSided != true) //Unside
+      {
+        this.$data.sidedChats = sendchat;
+        this.$data.chats = receivechat;
+      }
+      else //Side
+      {
+        this.$data.sidedChats = receivechat;
+        this.$data.chats = sendchat;
+      }
+
+    },
     minusChat(data){
       let index =  this.$data.chats.indexOf(data)
       this.$data.chats[index].isMinus = !this.$data.chats[index].isMinus;
       console.log('isMinus == ' + this.$data.chats[index].isMinus)
     },
     closeChat(data){
+      if (data.ptype == true)
+        this.$props.socket.emit('exit_room', {id: data.data.id})
       let index =  this.$data.chats.indexOf(data)
       this.$data.chats.splice(index,1)
     },
@@ -125,8 +210,30 @@ export default {
 </script>
 
 <style scoped>
+
+.messagesContainer{
+
+  width: 90%;
+  padding-left: 2.5%;
+  padding-right: 7.5%;
+}
+
+.messagesContainer::-webkit-scrollbar{
+  background-color: var(--fourth-color);
+  padding-left: 3px;
+}
+
+.messagesContainer::-webkit-scrollbar-thumb{
+  background-color: var(--main-color);
+  border-top: 2px solid var(--fourth-color);
+  border-right: 2px solid var(--fourth-color);
+  border-bottom: 2px solid var(--fourth-color);
+
+}
+
+
 .mainChatPop{
-  height: 410px;
+  height: 406px;
   width: auto;
   display: flex;
   flex-direction: row-reverse;
@@ -137,8 +244,14 @@ export default {
   pointer-events: none;
 }
 
-.chatPop{
+.chatPotDiv{
+  width: inherit;
   height: 100%;
+  z-index: 0;
+}
+
+.chatPop{
+  height: 400px;
   width: 275px;
   overflow: hidden;
   pointer-events: auto;
@@ -147,14 +260,16 @@ export default {
   margin-right: 15px;
   padding: 0;
   background-color: var(--main-color);
-  border-left: 3px solid transparent;
-  border-right: 3px solid transparent;
-  border-top: 3px solid transparent;
-  transition: transform 0.5s ease-in-out;
+  border: 3px solid transparent;
+  transition: all 0.3s ease-in-out;
+  transform: translateX(0) translateY(0);
+  z-index: 0;
 }
 
-.upChatPop{
-  transform: translateY(0);
+
+
+.sideChat{
+  z-index: 1;
 }
 
 .downChatPop{
@@ -163,28 +278,96 @@ export default {
 
 .chatsIconsDiv{
   width: 100px;
-  height: 100%;
+  height: inherit;
   transform: translateY(-25px);
   bottom: 15px;
+  z-index: 1;
 }
 
 .chatAddIco{
   width: 70px;
   height: 70px;
-  margin: 15px;
+  margin: 15px 15px;
   border-radius: 50%;
+  overflow: hidden;
+  transform: translateX(0) translateY(0);
+  opacity: 1;
   pointer-events: auto;
   transition: all  0.3s ease-in-out;
 }
 
-.chatSettIco{
+.chatSideIco{
   width: 50px;
   height: 50px;
-  margin: 15px 25px ;
+  margin: 15px 25px;
+}
+
+.chatSettIco{
+  width: 40px;
+  height: 40px;
+  margin: 15px 30px;
   overflow: hidden;
+  transform: translateX(0) translateY(0);
+  opacity: 1;
 }
 
 .chatAddIco:hover{
   transform:  scale(1.2);
+}
+
+.list{
+  transition: all 0.4s;
+}
+
+.list-enter-from, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateX(100vh);
+}
+
+.listIco{
+}
+
+.listIco-enter-from, .listIco-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateX(10vh);
+}
+
+.taMsg{
+  font-size:13px;
+  resize: none;
+  outline: none;
+  border-radius: 4px;
+  background-color: var(--third-color);
+  border: 1px solid;
+  border-color: transparent;
+}
+
+.taMsg:focus{
+  border-color: var(--secondary-color);
+  background-color: var(--fourth-color);
+}
+
+.taMsg::-webkit-scrollbar{
+  display: none;
+}
+
+.transition01{
+  transition: all 0.1s ease-in-out;
+}
+
+.transition02{
+  transition: all 0.2s ease-in-out;
+}
+
+.scaleHov12:hover{
+  transform: scale(1.2);
+}
+
+.sendEffect:hover{
+  transform: scale(1.2) rotate(35deg);
+}
+
+.sendEffect:active{
+  transform: scale(1.1) rotate(50deg) translateY(-50px) translateX(50px);
 }
 </style>
